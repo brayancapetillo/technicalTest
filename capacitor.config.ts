fdac095735c3477a1b3fde684fa144d02e7a54ee
Brayan Capetillo <brayancapetoccc@gmail.com>
2025-01-04 14:07:13 -0600

    :tada: feat(init): initialize technical test application

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'technicalTest',
+  webDir: 'www'
+};
+
+export default config;
