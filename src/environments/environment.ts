b307fba684a4aa1020ed92e1f8bdc27bfd2f6718
Brayan Capetillo <brayancapetoccc@gmail.com>
2025-01-06 02:54:05 -0600

    :wrench: chore(environment): update environment files to include firebase config variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyARrQQJWPhcyI5ixIvv6R-8_KXAq2J5KYA",
+    authDomain: "technicaltestbrayancapetillo.firebaseapp.com",
+    projectId: "technicaltestbrayancapetillo",
+    storageBucket: "technicaltestbrayancapetillo.firebasestorage.app",
+    messagingSenderId: "974056401004",
+    appId: "1:974056401004:web:4d0a95f6c53369f0891049"
+  }
 };
 
 /*
